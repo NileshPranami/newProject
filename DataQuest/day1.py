cd57ecb6b92bcc72a6c524f5ffe5addc9a1c7956
Nilesh <nk966331@gmail.com>
2020-01-13 07:38:12 +0530

    clean_and_convert function added

--- DataQuest/day1.py
+++ DataQuest/day1.py
@@ -8,3 +8,20 @@ for n in moma:
     n[2] = nationality
     if not nationality:
         n[2] = "Nationality Unknown"
+
+
+def clean_and_convert(date):
+    # check that we don't have an empty string
+    if date != "":
+        # move the rest of the function inside
+        # the if statement
+        date = date.replace("(", "")
+        date = date.replace(")", "")
+        date = int(date)
+    return date
+
+for item in moma:
+    value1 = clean_and_convert(item[3])
+    value2 = clean_and_convert(item[4])
+    item[3] = value1
+    item[4] = value2
