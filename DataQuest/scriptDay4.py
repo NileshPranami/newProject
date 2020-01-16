16ad1e69027e48ae9e1d3004b3f2298a66c46a63
Nilesh <nk966331@gmail.com>
2020-01-17 00:47:55 +0530

    format string added

--- DataQuest/scriptDay4.py
+++ DataQuest/scriptDay4.py
@@ -4,3 +4,9 @@ for item in decades:
         decade_frequency[item] = 1
     else:
         decade_frequency[item] += 1
+
+artist = "Pablo Picasso"
+birth_year = 1881
+
+output = "{0}'s birth year is {1}".format(artist,birth_year)
+print(output)
