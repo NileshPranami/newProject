11d40f56e8e760503e8585a08ff97a47bbe2acd8
Nilesh <nk966331@gmail.com>
2020-02-10 19:38:16 +0530

    Formated string with precision and comma seperated value added

--- DataQuest/scriptDay5.py
+++ DataQuest/scriptDay5.py
@@ -10,3 +10,14 @@ def artist_summary(name):
     count = artist_freq[name]
     print("There are {} artworks by {} in the data set".format(count,name))
 artist_summary("Henri Matisse")
+
+pop_millions = [
+    ["China", 1379.302771],
+    ["India", 1281.935991],
+    ["USA",  326.625791],
+    ["Indonesia",  260.580739],
+    ["Brazil",  207.353391],
+]
+
+for country in pop_millions:
+    print("The population of {} is {:,.2f} million".format(country[0],country[1]))
