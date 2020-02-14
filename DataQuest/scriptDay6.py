06d5291c779dea137e15a323b4d17312f5385962
Nilesh <nk966331@gmail.com>
2020-02-14 23:21:32 +0530

    init method learned

--- DataQuest/scriptDay6.py
+++ DataQuest/scriptDay6.py
@@ -4,3 +4,10 @@ class NewList(DQ):
 newlist = NewList()
 result = newlist.return_list([1,2,3])
 print(result)
+
+
+class NewList(DQ):
+    def __init__(self, initial_state):
+        self.data = initial_state
+my_list = NewList([1,2,3,4,5])
+print(my_list.data)
