9ce6ca2b3052ba0c2e729db93bdbc46605b8b8ae
Nilesh <nk966331@gmail.com>
2020-04-18 16:57:07 +0530

    Oops concept added

--- DataQuest/scriptDay7.py
+++ DataQuest/scriptDay7.py
@@ -7,3 +7,17 @@ class Example:
 
 obj = Example('Nilesh')
 obj.displayName()
+
+class CSEStudents:
+    stream = 'CSE'
+    def __init__(self, roll):
+        self.roll = roll
+
+    def setAddress(self, add):
+        self.add = add
+    def getAddress(self):
+        return self.add
+
+a = CSEStudents(13)
+a.setAddress('Siwan, Bihar')
+print(a.getAddress())
