d99cb528aa57a6d9357186c56456222293827ac1
Nilesh <nk966331@gmail.com>
2020-04-23 10:27:38 +0530

    Test class added

--- DataQuest/scriptDay7.py
+++ DataQuest/scriptDay7.py
@@ -21,3 +21,18 @@ class CSEStudents:
 a = CSEStudents(13)
 a.setAddress('Siwan, Bihar')
 print(a.getAddress())
+
+
+"""printing objects"""
+class Test:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def __repr__(self):
+        return "Value of Test that a is %s b is %s" % (self.a,self.b)
+    def __str__(self):
+        return "Value through str method is a : %s , b : %s"%(self.a,self.b)
+
+tst = Test(4,6)
+print(tst)
+print([tst])
